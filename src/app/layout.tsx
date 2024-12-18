72e2be90940bd3474e19d1ed954a119271ab469f
Arash Yeganeh <arashkosariyeganeh@gmail.com>
2024-12-18 15:04:29 +0330

    Lesson 1 (#2)
    * [INIT]
    
    * [REMOVE] unnecessary files and codes
    [ADD] vazirmatn font
    [ADD] `hello, friend!` example
    
    * [ADD] dynamic route example

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from 'next/font/google';
+import "./globals.css";
+import type { NextFont } from "next/dist/compiled/@next/font";
+
+const vazirmatn: NextFont = Vazirmatn({
+  subsets: ['latin', 'arabic'],
+  display: 'swap'
+})
+
+export const metadata: Metadata = {
+  title: "دکتر من",
+  description: "پلتفرم جامع جستجوی دکتر  و رزرو آنلاین",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body className={vazirmatn.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
